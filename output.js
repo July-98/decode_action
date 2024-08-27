a831c2f978504b9f4292509280d006d500d1ff16
action <action@github.com>
2024-08-27 06:38:57 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Tue Aug 27 2024 06:33:46 GMT+0000 (Coordinated Universal Time)
+//Tue Aug 27 2024 06:38:57 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 async function pusherTxt(_0x2cd64a, _0xa84494) {}

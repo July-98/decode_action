b1b75c3e842ad1ecc81f35c8bdaf615ac3163035
action <action@github.com>
2024-08-27 09:26:11 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,41 +1,47 @@
-//Tue Aug 27 2024 09:23:23 GMT+0000 (Coordinated Universal Time)
+//Tue Aug 27 2024 09:26:11 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-const {
-  post,
-  sleep
-} = require("./tool");
-let hosturl = "http://146.56.128.138:8003",
-  headers = {
-    sign: "dsadaskjhkfhasdfds546416dsada"
-  };
+const axios = require("axios");
 module.exports = {
-  async get_wx_read_num(_0xd86cc9, _0x33e892, _0xb986f4) {
-    let _0x1041ac = await new Promise(async (_0x2c51ed, _0x610f63) => {
-      let _0x3e86ff;
-      while (true) {
-        _0x3e86ff = await post(hosturl, null, {
-          url: _0xd86cc9,
-          key: _0x33e892,
-          wx_key: process.env.wx_key,
-          not_check: _0xb986f4
-        }, headers);
-        if (!_0x3e86ff) {
-          _0x2c51ed(false);
-          return;
-        }
-        if (_0x3e86ff.data.code == -2) {
-          await sleep(1000);
-        } else {
-          break;
-        }
-      }
-      if (_0x3e86ff) {
-        _0x2c51ed(_0x3e86ff.data);
-      } else {
-        _0x2c51ed(false);
-      }
+  getRandomInt(_0x238e08, _0x12d648) {
+    return Math.floor(Math.random() * (_0x12d648 - _0x238e08 + 1)) + _0x238e08;
+  },
+  async sleep(_0x17de59) {
+    return await new Promise(_0x593468 => setTimeout(_0x593468, _0x17de59));
+  },
+  async get(_0x5c0154, _0x30b73b, _0x55986c) {
+    let _0x4c8f63 = await new Promise(function (_0x46e61a, _0x4ec0e8) {
+      axios({
+        url: _0x5c0154,
+        method: "GET",
+        headers: _0x55986c,
+        params: _0x30b73b,
+        timeout: 10000
+      }).then(_0x4808d1 => {
+        _0x46e61a(_0x4808d1);
+      }).catch(_0x509725 => {
+        console.log(_0x5c0154, _0x509725.message);
+        _0x46e61a(false);
+      });
     });
-    return _0x1041ac;
+    return _0x4c8f63;
+  },
+  async post(_0x183806, _0x554671, _0x5a5a1f, _0x2be79e) {
+    let _0x57c0e0 = await new Promise(function (_0x3cb912, _0x3a488b) {
+      axios({
+        url: _0x183806,
+        method: "POST",
+        headers: _0x2be79e,
+        params: _0x554671,
+        data: _0x5a5a1f,
+        timeout: 10000
+      }).then(_0x35b9fc => {
+        _0x3cb912(_0x35b9fc);
+      }).catch(_0x164652 => {
+        console.log(_0x183806, _0x164652.message);
+        _0x3cb912(false);
+      });
+    });
+    return _0x57c0e0;
   }
 };
